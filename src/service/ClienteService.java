67c04cdf174d6d4328ac0b359c770b0591442a01
renato vargas <renato-vargas@outlook.com>
2019-06-01 22:53:17 -0300

    exercicio

---
+++ src/service/ClienteService.java
@@ -0,0 +1,28 @@
+package service;
+
+import java.io.IOException;
+
+import model.Cliente;
+import dao.ClienteDAO;
+
+
+public class ClienteService {
+	ClienteDAO dao = new ClienteDAO();
+	
+	public int criar(Cliente cliente) throws IOException {
+		return dao.criar(cliente);
+	}
+	
+	public void atualizar(Cliente cliente) throws IOException {
+		dao.atualizar(cliente);
+	}
+	
+	public void excluir(int id) throws IOException {
+		dao.excluir(id);
+	}
+	
+	public Cliente carregar(int id) throws IOException {
+		return dao.carregar(id);
+	}
+
+}
